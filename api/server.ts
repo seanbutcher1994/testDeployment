48daa50a606ff81594463bea914a6cbed9893c6c
seanbutcher1994 <110334815+seanbutcher1994@users.noreply.github.com>
2023-07-12 14:50:42 +0800

    blah

--- api/server.ts
+++ api/server.ts
@@ -1,17 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path"
 
 const app = express();
 dotenv.config();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
+app.use(express.static(path.join(__dirname, "../../client/build")));
+
+if (process.env.NODE_ENV === "production") {
+
+  app.get("*", (req, res) => {
+
+    res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+
+  });
+
+}
 
 app.listen(port, () => {
     console.log(`listening on PORT: ${port}`);
 });
+
 
 export default app;
 
